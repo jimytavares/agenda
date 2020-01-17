7af1ac683a893e2f6e196409d8776ab881490ab8
Jimy Dickson <jimytavares@hotmail.com>
2020-01-17 10:36:07 -0300

    enviando

---
+++ contatos/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+    # Create your models here.
+
+class Contato(models.Model):
+    nome_contato = models.CharField(max_length=100, verbose_name='Nome Completo')
+    telefone_1   = models.CharField(max_length=11, verbose_name='Telefone Principal')
+    telefone_2   = models.CharField(max_length=11, blank=True, null=True, verbose_name='Telefone Secundario')
+    email        = models.EmailField(verbose_name='Email')
+
+    def __str__ (self):
+        return self.nome_contato
---
+++ contatos/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.contato_list, name='contato_list'),
+]
